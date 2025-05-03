2089302de14c31c76581f8880cd436281bc2d1b8
Kaguya <167426134+XiaoyeQWQ@users.noreply.github.com>
2025-05-03 20:40:10 +0800

    Update ScriptLoader.lua

--- ScriptLoader.lua
+++ ScriptLoader.lua
@@ -1,1 +1,1 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "傻子贝利亚",Text = "我白名单掉了，你叫你妈呢",Icon = "rbxassetid://",Duration = 2,Callback = bindable,Button1 = "cnm",Button2 = "cnm"})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "傻子贝利亚",Text = "我白名单掉了，你叫你妈呢",Icon = "rbxassetid://",Duration = math.huge,Callback = bindable,Button1 = "cnm",Button2 = "cnm"})
